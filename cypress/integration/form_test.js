a49eca6f0475b426f94cba5023ee9a2afeace084
JenVest2020 <jenvest2019@gmail.com>
2020-06-10 22:02:46 -0500

    copied code given in slack by Emily

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,11 +1,11 @@
-describe('Form - testing our form inputs', function ()){
-    it('visit our localhost', function ()){
-        beforeEach(() => {
-            cy.visit('http://localhost:3000');
-        });
-        it('add text to inputs and submit form', function ({
-            cy.get('[data=cy="name"').type("Emily)
-        }))
-
-}
-}
+describe("Form - testing our form inputs", function () {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/");
+    });
+    it("add text to inputs and submit form", function () {
+        cy.get('[data-cy="name"]').type("Emily").should("have.value", "Emily");
+        cy.get('[data-cy="email"]')
+            .type("test@gmail.com")
+            .should("have.value", "test@gmail.com");
+    });
+});
