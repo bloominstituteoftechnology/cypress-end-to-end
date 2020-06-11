beca0d89dbc8fa0a959201fa186478d9e03dd609
JenVest2020 <jenvest2019@gmail.com>
2020-06-10 22:11:24 -0500

    copied second code group from Emilys paste in slak

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -7,5 +7,8 @@ describe("Form - testing our form inputs", function () {
         cy.get('[data-cy="email"]')
             .type("test@gmail.com")
             .should("have.value", "test@gmail.com");
-    });
-});
+        cy.get("[data-cy=motivation]")
+            .type("I want to help")
+            .should("have.value", "I want to help");
+        cy.get("#positions").select("Yard Work").should("have.value", "Yard Work");
+        cy.get('[type="checkbox"]').check().should("be.checked");
